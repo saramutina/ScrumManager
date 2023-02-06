c4acc85f83a10a622a50cd96e2f9003706a1784f
Katie Saramutina <saramutina@gmail.com>
2023-02-06 16:18:41 +0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Katie Saramutina on 06.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
