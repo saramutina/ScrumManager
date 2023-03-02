2e41098ab6ce4ac59e103b6755ba73d5ad20d751
Katie Saramutina <saramutina@gmail.com>
2023-03-02 04:22:51 +0400

    ✨ Add ding sound when it's next person's turn to talk

---
+++ ScrumManager/Models/AVPlayer.swift
@@ -0,0 +1,16 @@
+//
+//  AVPlayer.swift
+//  ScrumManager
+//
+//  Created by Katie Saramutina on 02.03.2023.
+//
+
+import Foundation
+import AVFoundation
+
+extension AVPlayer {
+    static let sharedDingPlayer: AVPlayer = {
+        guard let url = Bundle.main.url(forResource: "ding", withExtension: "wav") else { fatalError("Failed to find sound file.") }
+        return AVPlayer(url: url)
+    }()
+}
--- ScrumManager/Models/DailyScrum.swift
+++ ScrumManager/Models/DailyScrum.swift
@@ -57,7 +57,7 @@ extension DailyScrum {
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
     [
-        DailyScrum(title: "Hogwarts", attendees: ["Minerva", "Albus", "Severus", "Harry", "Hermionie", "Ron", "Salazar", "Helga", "Rowena", "Godric"], lengthInMinutes: 15, theme: .yellow),
+        DailyScrum(title: "Hogwarts", attendees: ["Minerva", "Albus", "Severus", "Harry", "Hermionie", "Ron", "Salazar", "Helga", "Rowena", "Godric"], lengthInMinutes: 1, theme: .yellow),
         DailyScrum(title: "Jedi Order", attendees: ["Anakin", "Obi Van", "Yoda", "Chewbacca", "Padmé"], lengthInMinutes: 10, theme: .indigo),
         DailyScrum(title: "Cyberlife", attendees: ["Connor", "Markus", "Kara", "Luther", "Hank", "Carl"], lengthInMinutes: 15, theme: .seafoam)
     ]
