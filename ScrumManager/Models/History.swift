9fb8cd1b67586f9223176c04cb4cab7636fa665f
Katie Saramutina <saramutina@gmail.com>
2023-03-02 20:04:05 +0400

    ✨ Add meeting history section in scrum detail view - it shows when this meeting has been held

---
+++ ScrumManager/Models/History.swift
@@ -0,0 +1,22 @@
+//
+//  History.swift
+//  ScrumManager
+//
+//  Created by Katie Saramutina on 02.03.2023.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    var lengthInMinutes: Int
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int = 5) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+    }
+}
