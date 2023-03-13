3f472ab775e1c4f9e7e1fa174b28d50fa93537d3
Katie Saramutina <saramutina@gmail.com>
2023-03-13 18:50:13 +0400

    ⚙️ Modify ScrumStore to use async/await pattern

--- ScrumManager/Models/ScrumStore.swift
+++ ScrumManager/Models/ScrumStore.swift
@@ -19,6 +19,20 @@ class ScrumStore: ObservableObject {
         .appendingPathComponent("scrums.data")
     }
     
+    // MARK: Async load()
+    static func load() async throws -> [DailyScrum] {
+        try await withCheckedThrowingContinuation { continuation in
+            load { result in
+                switch result {
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                case .success(let scrums):
+                    continuation.resume(returning: scrums)
+                }
+            }
+        }
+    }
+    
     static func load(completion: @escaping (Result<[DailyScrum], Error>) -> Void) {
         DispatchQueue.global(qos: .background).async {
             do {
@@ -36,6 +50,22 @@ class ScrumStore: ObservableObject {
             } catch {
                 DispatchQueue.main.async {
                     completion(.failure(error))
+                }
+            }
+        }
+    }
+    
+    //MARK: Async save()
+    /// discardable result disables warnings about unused return value
+    @discardableResult
+    static func save(scrums: [DailyScrum]) async throws -> Int {
+        try await withCheckedThrowingContinuation { continuation in
+            save(scrums: scrums) { result in
+                switch result {
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                case .success(let scrumsSaved):
+                    continuation.resume(returning: scrumsSaved)
                 }
             }
         }
--- ScrumManager/ScrumManagerApp.swift
+++ ScrumManager/ScrumManagerApp.swift
@@ -16,21 +16,20 @@ struct ScrumManagerApp: App {
         WindowGroup {
             NavigationView {
                 ScrumsView(scrums: $store.scrums) {
-                    ScrumStore.save(scrums: store.scrums) { result in
-                        if case .failure(let error) = result {
-                            fatalError(error.localizedDescription)
+                    Task {
+                        do {
+                            try await ScrumStore.save(scrums: store.scrums)
+                        } catch {
+                            fatalError("Error saving scrums.")
                         }
                     }
                 }
             }
-            .onAppear {
-                ScrumStore.load { result in
-                    switch result {
-                    case .failure(let error):
-                        fatalError(error.localizedDescription)
-                    case .success(let scrums):
-                        store.scrums = scrums
-                    }
+            .task {
+                do {
+                    store.scrums = try await ScrumStore.load()
+                } catch {
+                    fatalError("Error saving scrums.")
                 }
             }
         }
